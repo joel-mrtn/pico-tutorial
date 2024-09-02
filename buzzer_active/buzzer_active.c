f002ffbc89449427404579ca281cbadce2d2e463
Joel Martin <64128660+joel-mrtn@users.noreply.github.com>
2024-09-02 11:53:16 +0200

    Add active buzzer program

---
+++ buzzer_active/buzzer_active.c
@@ -0,0 +1,23 @@
+#include "pico/stdlib.h"
+#include <stdio.h>
+
+#define PIN_BUZZER 15
+#define PIN_BUTTON 16
+
+int main(void) {
+    gpio_init(PIN_BUZZER);
+    gpio_init(PIN_BUTTON);
+
+    gpio_set_dir(PIN_BUZZER, GPIO_OUT);
+    gpio_set_dir(PIN_BUTTON, GPIO_IN);
+
+    gpio_put(PIN_BUZZER, false);
+
+    while (true) {
+        if (gpio_get(PIN_BUTTON) == false) {
+            gpio_put(PIN_BUZZER, true);
+        } else {
+            gpio_put(PIN_BUZZER, false);
+        }
+    }
+}
