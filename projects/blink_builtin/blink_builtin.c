27d7cd8c44b0901ce6158cfec89e187c4e6c8977
Joel Martin <64128660+joel-mrtn@users.noreply.github.com>
2024-09-05 10:37:52 +0200

    Simplify blink builtin program

--- projects/blink_builtin/blink_builtin.c
+++ projects/blink_builtin/blink_builtin.c
@@ -4,18 +4,14 @@
 #define LED_DELAY_MS 1000
 
 int main() {
-    stdio_init_all();
-
     const int rc = cyw43_arch_init();
     hard_assert(rc == PICO_OK);
 
     while (true) {
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-        puts("LED ON");
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, true);
         sleep_ms(LED_DELAY_MS);
 
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-        puts("LED OFF");
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, false);
         sleep_ms(LED_DELAY_MS);
     }
 }
